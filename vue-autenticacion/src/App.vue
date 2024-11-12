3e121676da0240eaaef784a9ba6c56837c4fe100
javiergp23 <javgon2306@gmail.com>
2024-11-12 19:09:16 -0300

    config app

--- vue-autenticacion/src/App.vue
+++ vue-autenticacion/src/App.vue
@@ -1,5 +1,23 @@
 <script>
-import firebase from "../firebaseConfig.js";
+  import firebase from "../firebaseConfig.js";
+
+  export default {
+    data(){
+      return {
+        email: "",
+        password: "",
+      }
+    },
+    methods: {
+      async register(){
+        try{
+
+        }catch(error){
+          console.error(error, 'Erro al crear el usuario')
+        }
+      }
+    }
+  }
 </script>
 
 <template>
--- vue-autenticacion/src/auth.js
+++ vue-autenticacion/src/auth.js
@@ -1,2 +1,7 @@
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
+import firebaseApp from "../firebaseConfig.js";
+
+const auth = getAuth(firebaseApp); 
+
+export {auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged}
