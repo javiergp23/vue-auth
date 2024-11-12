78deb8734370edee8ac31fde89d0723406d454c1
javiergp23 <javgon2306@gmail.com>
2024-11-12 20:14:23 -0300

    fix routes

--- vue-autenticacion/src/App.vue
+++ vue-autenticacion/src/App.vue
@@ -6,8 +6,7 @@
 <template>
   
   <div>
-   <router-link to="/">Home</router-link>
-   <router-link to="/login">Login</router-link>
+   <router-link to="/">Login</router-link>
    <router-link to="/register">Register</router-link>
   </div>
  
